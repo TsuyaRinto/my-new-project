667f053c11cf0194f5a1c8500471818505abb816
TsuyaRinto <r.tsuya@mitsui.com>
2024-12-03 17:37:47 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+
+// ボタンを取得
+const button = document.getElementById('clickMeButton');
+
+// ボタンがクリックされたときのイベントを設定
+button.addEventListener('click', function() {
+  alert('Button was clicked!');
+});
