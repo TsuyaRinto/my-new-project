e48c357a648385a7c3fcbcf0766835531a6d9320
TsuyaRinto <r.tsuya@mitsui.com>
2024-12-03 18:09:45 +0900

    Add Node.js project and GitHub Actions workflow

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-// index.js
-
-// ボタンを取得
-const button = document.getElementById('clickMeButton');
-
-// ボタンがクリックされたときのイベントを設定
-button.addEventListener('click', function() {
-  alert('Button was clicked!');
-});
+console.log("Hello, Node.js!");
